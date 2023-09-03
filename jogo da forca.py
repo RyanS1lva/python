5e21c9e85c41152929a2fcaac0d550e076d8d067
Ryan Silva <ryan1da1silva@gmail.com>
2023-09-03 12:08:47 -0300

    principal

--- jogo da forca.py
+++ jogo da forca.py
@@ -9,22 +9,26 @@ print('_________ \n'
 letra_palpite = []
 letra_acertada = []
 erros = 0
-
+acertos = 0
 while True:
     palavra_secreta = 'rato'
     letra_digitada = str(input('\nDigite uma letra: '))
 
-    if letra_digitada in letra_palpite:
-        print(f'A letra "{letra_digitada}" já foi digitada!')
+    if letra_digitada.isalpha():
+        if letra_digitada in letra_palpite:
+            print(f'A letra "{letra_digitada}" já foi digitada!')
 
-    if letra_digitada in palavra_secreta and letra_digitada not in letra_palpite:
-        letra_acertada.append(letra_digitada)
+        if letra_digitada in palavra_secreta and letra_digitada not in letra_palpite:
+            letra_acertada.append(letra_digitada)
+            acertos += 1
+        
+        if letra_digitada not in palavra_secreta and letra_digitada not in letra_palpite:
+            erros += 1
+        
         letra_palpite.append(letra_digitada)
-
-    
-    if letra_digitada not in palavra_secreta and letra_digitada not in letra_palpite:
-        erros += 1
-    
+        
+    else:
+        print('Digite uma letra, por favor!')
     
     if erros == 1:
         print('\n_________ \n'
@@ -58,3 +62,7 @@ while True:
 
         else:
             print('_', end='')
+    
+    if acertos == len(palavra_secreta):
+        print(f'\nParabéns, você ganhou, a palavra era {palavra_secreta}!')
+        break
