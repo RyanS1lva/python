1a384924f4f15cc20461c6bf32d5b7715e7dc1ca
Ryan Silva <ryan1da1silva@gmail.com>
2023-09-27 21:02:14 -0300

    principal

--- list comprehension.py
+++ list comprehension.py
@@ -1,4 +1,9 @@
-numeros_pares = [numero_par for numero_par in range(11) if numero_par % 2 == 0]
-print(f'Números pares de zero a dez: {numeros_pares}')
-numeros_impares = [numero_impar for numero_impar in range(11) if numero_impar % 2 != 0]
-print(f'Número impares de zero a dez: {numeros_impares}')
+numeros_lista = []
+for numero in range(10):
+    numeros_lista.append(numero) 
+
+# Ou utilizando list comprehension; o resultado em ambos os casos é o mesmo
+
+lista_comprehension = [numero for numero in range(10)]
+
+
---
+++ pares e impares.py
@@ -0,0 +1,4 @@
+numeros_pares = [numero_par for numero_par in range(11) if numero_par % 2 == 0]
+print(f'Números pares de zero a dez: {numeros_pares}')
+numeros_impares = [numero_impar for numero_impar in range(11) if numero_impar % 2 != 0]
+print(f'Número impares de zero a dez: {numeros_impares}')
